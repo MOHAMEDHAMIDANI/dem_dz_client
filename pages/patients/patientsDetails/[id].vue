c2bd3829f39d48fce978eead38bc87a27ba50e22
MrMH <mohamidani1@gmail.com>
2024-07-23 17:59:47 +0300

    functionality of the profile

--- pages/patients/patientsDetails/[id].vue
+++ pages/patients/patientsDetails/[id].vue
@@ -5,57 +5,53 @@
                 <ul
                     class="flex-column h-full space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
                     <li>
-                        <a href="#"
-                            class="inline-flex items-center justify-around px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
+                        <a href="#" @click.prevent="setActiveTab('profile')"
+                            :class="['inline-flex items-center px-4 py-3 rounded-lg w-full', activeTab === 'profile' ? 'text-white bg-blue-700 dark:bg-blue-600' : 'bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700']"
                             aria-current="page">
                             <svg class="w-4 h-4 me-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                             </svg>
-                            <h3>
-                            Profile
-                            </h3>
+                            <h3>Profile</h3>
                         </a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="inline-flex items-center justify-around px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <a href="#" @click.prevent="setActiveTab('progress')"
+                            :class="['inline-flex items-center px-4 py-3 rounded-lg w-full', activeTab === 'progress' ? 'text-white bg-blue-700 dark:bg-blue-600' : 'bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700']">
                             <Icon name="carbon:request-quote" size="20" class="mr-1" />
                             <h3 class="capitalize">progress</h3>
                         </a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="inline-flex items-center justify-around px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <a href="#" @click.prevent="setActiveTab('examination')"
+                            :class="['inline-flex items-center px-4 py-3 rounded-lg w-full', activeTab === 'examination' ? 'text-white bg-blue-700 dark:bg-blue-600' : 'bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700']">
                             <Icon name="healthicons:i-exam-multiple-choice-outline" size="20" class="mr-1" />
-                            <h3 class="capitalize">
-                            examination
-                            </h3>
+                            <h3 class="capitalize">examination</h3>
                         </a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="inline-flex items-center justify-around px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <a href="#" @click.prevent="setActiveTab('treatment')"
+                            :class="['inline-flex items-center px-4 py-3 rounded-lg w-full', activeTab === 'treatment' ? 'text-white bg-blue-700 dark:bg-blue-600' : 'bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700']">
                             <Icon name="material-symbols:health-and-safety-rounded" size="20" class="mr-1" />
                             <h3 class="capitalize">treatment</h3>
                         </a>
                     </li>
                 </ul>
-                <div v-if="false"
-                    class="p-6 bg-gray-50 text-medium  text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
-                    <div class="w-full flex justify-between items-center">
-                        <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Profile</h3>
+                <div v-if="activeTab === 'profile'"
+                    class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
+                    <div class="w-full flex justify-between items-center mb-5">
+                        <h3 class="text-lg font-bold text-gray-900 dark:text-white  capitalize">Profile</h3>
                         <Icon name="material-symbols:person-edit-outline-sharp" size="35" class="cursor-pointer" />
                     </div>
                     <div class="w-full h-full grid grid-cols-3">
                         <div class="flex flex-col pb-3 col-start-1">
-                            <dt class="mb-1 text-gray-500 md:text-lg capitalize dark:text-white">full name</dt>
+                            <dt class="mb-1 text-gray-500 md:text-lg capitalize dark:text-white font-medium">full name</dt>
                             <dd class="text-lg font-semibold">mohamed hamidani </dd>
                         </div>
                         <div class="flex flex-col pb-3 col-start-2">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">gender</dt>
-                            <dd class="text-lg font-semibold">yourname@flowbite.com</dd>
+                            <dd class="text-lg font-semibold">male</dd>
                         </div>
                         <div class="flex flex-col pb-3 col-start-3">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">date of birth</dt>
@@ -70,15 +66,15 @@
                         </div>
                         <div class="flex flex-col pb-3 col-start-1">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">section</dt>
-                            <dd class="text-lg font-semibold">Bl2 </dd>
+                            <dd class="text-lg font-semibold">emergency </dd>
                         </div>
                         <div class="flex flex-col pb-3 col-start-2">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">room</dt>
-                            <dd class="text-lg font-semibold">yourname@flowbite.com</dd>
+                            <dd class="text-lg font-semibold">02</dd>
                         </div>
                         <div class="flex flex-col pb-3 col-start-3">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">bed</dt>
-                            <dd class="text-lg font-semibold">2</dd>
+                            <dd class="text-lg font-semibold">02</dd>
                         </div>
                         <div class="flex flex-col pb-3 col-start-1">
                             <dt class="mb-1 text-gray-500 md:text-lg dark:text-white capitalize">sick guard </dt>
@@ -94,21 +90,22 @@
                         </div>
                     </div>
                 </div>
-                <div
-                    class="p-6 bg-gray-50 text-medium  text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
-                    <div class="mb-1">
+                <div v-if="activeTab === 'progress'"
+                    class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
+                    <form class="mb-1">
                         <div class="flex justify-between items-center mb-2">
-                        <label for="message" class="inline-block text-sm font-medium text-gray-900 dark:text-white capitalize">New progress</label>
-                        <button type="button"
-                            class="capitalize focus:outline-none outline-none text-white bg-blue-700 hover:bg-blue-900  focus:ring-2 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5  mx-2 flex items-center">
-                            submit
-                        </button>
+                            <label for="message"
+                                class="inline-block text-sm font-medium text-gray-900 dark:text-white capitalize">New
+                                progress</label>
+                            <button type="submit"
+                                class="capitalize focus:outline-none outline-none text-white bg-blue-700 hover:bg-blue-900 focus:ring-2 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 mx-2 flex items-center">
+                                submit
+                            </button>
                         </div>
-                        <textarea id="message" rows="4"
+                        <textarea id="message" rows="4" required
                             class="block p-2.5 mb-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Write the new progress you saw here "></textarea>
-
-                    </div>
+                    </form>
                     <div class="w-full flex justify-between items-center">
                         <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2 capitalize">progress</h3>
                     </div>
@@ -152,20 +149,34 @@
                                 web components and interactive elements built on top of Tailwind CSS.</p>
                         </li>
                     </ol>
-
-
+                </div>
+                <div v-if="activeTab === 'examination'"
+                    class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
+                    <div class="w-full flex justify-between items-center">
+                        <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2 capitalize">examination</h3>
+                        <Icon name="material-symbols:person-edit-outline-sharp" size="35" class="cursor-pointer" />
+                    </div>
+                </div>
+                <div v-if="activeTab === 'treatment'"
+                    class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg max-w-6xl w-full">
+                    <div class="w-full flex justify-between items-center">
+                        <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2 capitalize">treatment</h3>
+                        <Icon name="material-symbols:person-edit-outline-sharp" size="35" class="cursor-pointer" />
+                    </div>
                 </div>
             </div>
-
-
         </div>
     </MainLay>
 </template>
 
 <script setup lang="ts">
-import MainLay from '~/layouts/mainLay.vue';
+import MainLay from '~/layouts/mainLay.vue'
 
+const activeTab = ref('profile')
 
+const setActiveTab = (tab: string) => {
+    activeTab.value = tab
+}
 </script>
 
 <style scoped></style>
